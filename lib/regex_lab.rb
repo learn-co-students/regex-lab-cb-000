3fbf63b2e5b203189f5da962b6232fbeeadbe7b6
Derek Kaknes <derek.kaknes@gmail.com>
2016-12-15 00:59:42 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  !!word.match(/^[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/^[A-Z].*\W$/)
 end
 
 def valid_phone_number?(phone)
-
+  phone.gsub(/\D/, '').length == 10
 end

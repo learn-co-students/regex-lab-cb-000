6fe6134fac2050993a87b7927200224bb2f3d973
James Wilhelm <hello@jamesaverywilhelm.com>
2017-03-01 22:17:12 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,8 +13,7 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   text.match(/^[A-Z].*\W$/) ? true : false
 end
-#["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
-#["28894546", "(718)891-13135", "234 43 9978", "(800)IloveNY"]
+
 def valid_phone_number?(phone)
   phone.match(/^\(?\d{3}[\)\s]?\d{3}[-\s]?\d{4}$/) ? true : false
 end

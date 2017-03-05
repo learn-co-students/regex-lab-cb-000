08f2c525c451e16ef8b1f9bde3dd46ff43385e18
Kobie Jones <kobiedomo@gmail.com>
2017-03-05 05:58:46 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/\A[aeiouy]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+   text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.split().grep(/^\w{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].+[.?!]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\W?\d{3}\W?\d{3}\W?\d{4}$/) ? true : false
 end

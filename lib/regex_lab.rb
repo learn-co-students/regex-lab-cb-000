428aee51441f0a61e930c47f9f86dba01e819b97
Gabriel Constantin Staicu <gabriel_staicu@yahoo.com>
2017-03-28 11:48:33 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,29 @@
-def starts_with_a_vowel?(word)
+#require "pry"
+#
+#match = %w{ afoot Excellent incredible Absolute unreal Inconceivable octopus }
 
+def starts_with_a_vowel?(word)
+  !word.match(/\b([aeiou]|[AEIOU])[a-z]+/).nil?
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
+#match.each do |word|
+#  starts_with_a_vowel?(word)
+#end
+#
+#binding.pry
 
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\b[un][a-z]+[ing]/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !text.match(/^[A-Z].+\./).nil?
 end
 
 def valid_phone_number?(phone)
-
+  !phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/).nil?
 end

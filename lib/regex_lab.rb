213d9d27f3073e05dda7d3cd04c4cb09f8be8179
Daniel Justus <daniel.justus32@gmail.com>
2017-08-19 22:36:31 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,38 @@
 def starts_with_a_vowel?(word)
-
+  if word =~ /\A[aeiouAEIOU]\w+/
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text =~ /\A[A-Z].+[.?!]\z/
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  case phone
+  when /\(\d{3}\)\d{3}-\d{4}/
+    true
+  when /\(\d{3}\)\d{3}\d{4}/
+    true
+  when /\d{3}\s\d{3}\s\d{4}/
+    true
+  when /\d{10}/
+    true
+  else
+    false
+  end
 end

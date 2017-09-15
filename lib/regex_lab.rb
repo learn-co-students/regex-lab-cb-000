5849071ecb3bc74c8ac86ef9fd66b8bc6af5f449
nikifi <nikifiona@gmail.com>
2017-09-15 16:43:04 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,29 @@
 def starts_with_a_vowel?(word)
 
+ word.downcase.match(/\A[aeiou]/) == nil ? false : true
+	
 end
 
+puts starts_with_a_vowel?("Excellent")
+
 def words_starting_with_un_and_ending_with_ing(text)
+	text.scan(/un\w+ing\b/)
+
 
 end
 
 def words_five_letters_long(text)
+	text.scan(/\b\w{5}\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+	text.match(/^[A-Z].+[\.!?]$/) ? true : false
 
 end
 
 def valid_phone_number?(phone)
+	
+  phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
 
 end

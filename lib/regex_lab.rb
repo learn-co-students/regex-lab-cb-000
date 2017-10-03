8f9fc0e98574cfbc883f689dbb0e31f9039f87c5
Michael Wahlin <wahlin@gmail.com>
2017-10-03 12:43:00 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
 def starts_with_a_vowel?(word)
-
+  return true if word.match(/^[aeiouAEIOU]/)
+  false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  return true if text.match(/^[A-Z].*[.!]$/)
+  false
 end
 
 def valid_phone_number?(phone)
-
+  return true if phone.match(/\d{3}.*\d{3}.*\d{3}/)
+  false
 end

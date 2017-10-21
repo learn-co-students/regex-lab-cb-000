0e5074897b6fbe3f0a42c201c03afd23cdf759db
David Kuruvilla <dapawn@gmail.com>
2017-10-21 13:10:12 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
-def starts_with_a_vowel?(word)
+require "pry"
 
+def starts_with_a_vowel?(word)
+  word.match(/^[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.split(" ").grep(/^\w{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].*\W$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/(\d){3}.*(\d){3}.*(\d){4}/) ? true : false
 end

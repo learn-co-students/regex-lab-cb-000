73512aeee9e6dbfdd65f749454b4ce388dfeac60
Jawann <jawann@gmail.com>
2017-12-02 03:44:39 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  !!/^[aeiou]/i.match(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un.*?ing/i)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!/^[A-Z].*?\W$/.match(text)
 end
 
 def valid_phone_number?(phone)
-
+  !!/[\(|\s]{,1}\d{3}[\)|\s]{,1}\d{3}[\-|\s]{,1}\d{4}/.match(phone)
 end

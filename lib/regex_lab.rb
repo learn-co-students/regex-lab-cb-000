8b6c5da9d2ed32cb3f3fd52ba24c346b4cc74071
Richard Loera <richexp9@gmx.com>
2018-01-17 05:19:08 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,29 @@
-def starts_with_a_vowel?(word)
 
+
+words_string = "unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary"
+
+def starts_with_a_vowel?(word)
+  boolean = false
+  word.scan(/[aeiouAEIOU]/).each { |letter|
+    if word[0] == letter
+      boolean = true
+    end
+  }
+  boolean
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing/)
 end
 
-def words_five_letters_long(text)
+# print words_starting_with_un_and_ending_with_ing(words_string)
 
+def words_five_letters_long(text)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/\b^[A-Z]\w[.,\/#!$%\^&\*;:{}=\-_`~()]/)
 end
 
 def valid_phone_number?(phone)

38ede54c396cf65237d36c2b53ae50caef5a67e7
David Hopkins <dlhopkins2@gmail.com>
2018-01-17 02:07:22 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/^[aeiouAEIOU]\w/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].+[\.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\d{3}\W?\d{3}\W?\d{4}/) ? true : false
 end

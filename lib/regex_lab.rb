15cc33813ebed588e522fc68e9eea1daf975f3f1
David Hopkins <dlhopkins2@gmail.com>
2018-01-17 02:10:36 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-  word.match(/^[aeiouAEIOU]\w/) ? true : false
+  word.match(/^[aeiouAEIOU]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un+\w+ing/)
+  text.scan(/un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\b[a-z]{5}\b/)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/\d{3}\W?\d{3}\W?\d{4}/) ? true : false
+  phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
 end

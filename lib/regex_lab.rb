0ed0fec87ec0bcf3aa24a51a636fbb35aa3653cf
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-11 22:35:02 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,33 @@
 def starts_with_a_vowel?(word)
-
+  if word.scan(/^[aeiou]|[AEIOU]/) == []
+    false
+  else
+    true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b.{5}\s/).collect do |word|
+    word.chomp(' ')
+  end
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.scan(/\A[A-Z].*[,.;:]\z/) == []
+    false
+  else
+    true
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.scan(/([-()+\s]*\d){10}/) == []
+    false
+  else
+    true
+  end
 end

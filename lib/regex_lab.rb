af82ee7186e0d7574fc201d74d3bb593f035ab22
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-18 18:09:15 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,18 @@
 def starts_with_a_vowel?(word)
-
-end
+  !(word.match(/^[aeiou]\w+/i).nil?)
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !(text.match(/^[A-Z].*[\.?!]$/).nil?)
 end
 
 def valid_phone_number?(phone)
-
+  !(phone.match(/^[(+]?(\d+)[-\s)]?(\d+)[-\s]?(\d+)$/).nil?)
 end

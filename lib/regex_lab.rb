5be0a65a0fa0535963e8611123de90b1fb6967a2
Felice Forby <feliceforby@gmail.com>
2018-04-27 23:43:28 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/\A[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun[a-z]*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].*[.?!]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  # Allow parentheses for area code
+  # Allow spaces or dashes between phone number groups
+  phone.match(/^\(*\d{3}\)*\s*-*\d{3}\s*-*\d{4}$/) ? true : false
 end

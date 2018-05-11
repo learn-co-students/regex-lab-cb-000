5122bdabed7c3dc2374e375be3dac5526ae28b6b
Phoenix <phoenix@anecso.com>
2018-05-11 05:31:17 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
 def starts_with_a_vowel?(word)
-
+  word.scan(/\b[aeiou]/i) != [] ? TRUE : FALSE
 end
+
+
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun*\w*ing\b/i)
 end
+
+
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/i)
 end
+
+
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/\b[A-Z]{1}\w*[[:punct:]]\W/) != [] ? TRUE : FALSE
 end
 
-def valid_phone_number?(phone)
 
+
+def valid_phone_number?(phone)
+  digits_array = phone.scan(/\d/)
+  digits_array.size == 10 ? TRUE : FALSE
 end

7c26db48abc89abdd5a0955200acbf68c30df1d6
Adam Urbina <adamurbina@gmail.com>
2018-06-02 17:03:20 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+    word.scan(/^[aeiouAEIOU]/).size == 0 ? false : true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un[a-z]*ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match(/^[A-Z].*\W$/) == nil ? false : true
 end
 
 def valid_phone_number?(phone)
-
+    phone.scan(/\d/).size == 10 ? true : false
 end

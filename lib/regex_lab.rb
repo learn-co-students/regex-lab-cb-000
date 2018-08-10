511ef1d276decfce462677fe9e28468258cd2d7e
Yosia Hadisusanto <yosia.hadisusanto@gmail.com>
2018-08-10 11:31:10 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
-end
+  if word.match(/\b[aiueoAIUEO]/).nil?
+    false
+  else 
+    true
+  end
+end 
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun.*?ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*\.$/).nil?
+    false
+  else 
+    true
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/).nil?
+    false
+  else
+    true
+  end
 end

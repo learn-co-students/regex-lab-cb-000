f358b3009fda02bdb39cc5aabf5e4430d5a5ac02
Cedric David <cedric.david@btopenworld.com>
2018-09-03 04:36:45 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,39 +1,28 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-  a = word.upcase.scan(/\b[AEIOU]\w*\b/)
-  letters = word.split("")
-  if a !=[]
-    return true
-  else
-    false
-  end
+   a = word.scan(/\b[aeiou]/i)
+   if a == []
+     false
+   else
+     true
+   end
 end
 
+
 def words_starting_with_un_and_ending_with_ing(text)
-  array = []
-  array = text.scan(/\bun\w*\ing\b/)
-  array
+ text.scan(/\bun\w*\ing\b/i)
+
+
 end
 
 def words_five_letters_long(text)
-  array=[]
-  array = text.scan(/\b\w{5}\b/)
-  array
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  sentence = text.match(/\A[A-Z].*\./)
-  if sentence != nil
-    return true
-  else
-    return false
-  end
+  text.match(/\A[A-Z].*\./) !=  nil ? true : false
 end
 
 def valid_phone_number?(phone)
-  number = phone.scan(/\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*/)
-  if number != []
-    true
-  else
-    false
-  end
+  phone.match(/\D?\d{3}\D?\d{3}\D?\d{4}$/) !=  nil ? true : false
 end

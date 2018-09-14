7012fcb2aed708592b13cda02713d92f85ede7ca
Sean Martin <sean.martin@gmail.com>
2018-09-14 00:55:28 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  %w(a e i o u).include?(word[0].downcase)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.split(" ").grep(/^un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !text.scan(/^[A-Z].*\W$/).empty?
 end
 
 def valid_phone_number?(phone)
-
+  !phone.scan(/^\d{10}|^\(\d{3}\)\d{3}-\d{4}|^\(\d{3}\)\d{7}|^\d{3}\s\d{3}\s\d{4}/).empty?
 end

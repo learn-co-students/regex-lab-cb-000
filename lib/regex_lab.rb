25585ffab6e699ae298da6e20026dc82e4412b01
Pearl Zeng <zhuzeng@umich.edu>
2018-09-19 09:18:31 -0400

    Update regex_lab.rb

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/\(?\d{3}[) ]?\d{3}[- ]?\d{4}/) #not working yet
+  phone.match(/\(?\d{3}[) ]?\d{3}[- ]?\d{4}/)
 end
